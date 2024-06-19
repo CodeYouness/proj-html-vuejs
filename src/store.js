a9ee5a9ce0a6d85bd125ed18e162cbd2bace65a6
CodeYouness <ylijassi@gmail.com>
2024-06-19 12:33:17 +0200

    added on store, layout for card

--- src/store.js
+++ src/store.js
@@ -1,5 +1,103 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    animeList: 
+    animeList: [
+        {
+            "id": 1,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 2,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 3,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 4,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 5,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 6,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 7,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 8,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 9,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 10,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 11,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        },
+        {
+            "id": 12,
+            "categoria": "",
+            "titolo": "",
+            "data": "",
+            "descrizione": "",
+            "link": ""
+        }
+    ]
+
 });
