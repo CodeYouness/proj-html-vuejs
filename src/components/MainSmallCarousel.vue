93aa7b8f60d956f4a6162ad4899e9372e3f77931
CodeYouness <ylijassi@gmail.com>
2024-06-19 17:19:48 +0200

    added methods for img path

--- src/components/MainSmallCarousel.vue
+++ src/components/MainSmallCarousel.vue
@@ -1,14 +1,40 @@
 <script>
+import { store } from "../store";
+
 export default {
     data() {
         return {
+            store
         }
     },
-}
+    components: {
+        store
+    },
+    methods: {
+        getConsoleLog() {
+            console.log(this.store.animeList[0].path)
+        },
+        getImagePath : function (img) {
+                return new URL(img , import.meta.url).href;
+            }
+        }
+    }
+    created() {
+        this.getConsoleLog()
+    }
+
 </script>
 
 <template>
-<div>MainSmallCarousel</div>
+    <div class="container-fluid d-flex align-items-center">
+        <div class="row overflow-x-auto">
+            <ul class="d-flex flex-nowrap">
+                <li v-for="pic in store.animeList" :key="pic.id" class="card">
+                    <img :src="" alt="">
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
