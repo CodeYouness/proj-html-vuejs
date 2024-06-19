be4d2dea24d8cc27d94252c980c78bfcd1eefc24
CodeYouness <ylijassi@gmail.com>
2024-06-19 23:08:43 +0200

    added function to get first four object added function for next button on carousel

--- src/components/MainSmallCarousel.vue
+++ src/components/MainSmallCarousel.vue
@@ -4,18 +4,34 @@ import { store } from "../store";
 export default {
     data() {
         return {
-            store
+            store,
+            currentIndex: 4,
+            currentArray: []
         }
     },
     components: {
         store
     },
     methods:{
-        getImagePath : function (img) {
+        getImagePath(img) {
             return new URL(img , import.meta.url).href;
+        },
+        getFourCard(animeList) {
+            this.currentArray = animeList.slice(0, 4);
+            console.log(this.currentArray)
+        },
+        nextPicCarousel(){
+        this.currentArray.shift();
+        this.currentArray.push(this.store.animeList[this.currentIndex]);
+        if (this.currentIndex > 12) {
+            this.currentIndex = this.currentIndex + 1;
+        } else {
+            this.currentIndex = 0
+        }
         }
     },
     created() {
+        this.getFourCard(this.store.animeList)
     }
 }
 
@@ -25,7 +41,7 @@ export default {
     <div class="container-fluid d-flex align-items-center">
         <div class="row overflow-x-auto">
             <div class="d-flex flex-nowrap">
-                <li v-for="pic in store.animeList" :key="pic.id" class="card">
+                <li v-for="pic in currentArray" :key="pic.id" class="card">
                     <img :src="getImagePath(`.${pic.path}`)" :alt="pic.id">
                 </li>
             </div>
