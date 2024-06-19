43222f3ed846342bd113d732e7dbd621167813cf
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-19 16:09:27 +0200

    test

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,9 +2,64 @@
 export default {
     data() {
         return {
-            
+            startedLinks: [
+                {
+                    title: 'resources'
+                },
+                {
+                    title: 'tutorials'
+                },
+                {
+                    title: 'examples'
+                },
+                {
+                    title: 'docs'
+                }
+            ],
+            aboutLinks: [
+                {
+                    title: 'stories'
+                },
+                {
+                    title: 'community'
+                },
+                {
+                    title: 'blog'
+                },
+                {
+                    title: 'brand assets'
+                }
+            ],
+            featuresLinks: [
+                {
+                    title: 'overview'
+                },
+                {
+                    title: 'design'
+                },
+                {
+                    title: 'code'
+                },
+                {
+                    title: 'collaborate'
+                }
+            ],
+            quickLinks:[
+                {
+                    title: 'stories'
+                },
+                {
+                    title: 'communityt'
+                },
+                {
+                    title: 'blog'
+                },
+                {
+                    title: 'brand assets'
+                }
+            ]
         }
-    },
+    }
 }
 </script>
 
@@ -15,4 +70,8 @@ export default {
 <style scoped lang="scss">
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
+
+h1 {
+    background-color: $primary-color;
+}
 </style>
