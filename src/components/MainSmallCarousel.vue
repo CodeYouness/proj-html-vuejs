f6c35055b403d1887e84d6ce7e7e3eeed52eb326
CodeYouness <ylijassi@gmail.com>
2024-06-20 12:56:25 +0200

    updated function for carousel

--- src/components/MainSmallCarousel.vue
+++ src/components/MainSmallCarousel.vue
@@ -19,17 +19,21 @@ export default {
         },
         getFourCard(animeList) {
             this.currentArray = animeList.slice(0, 4);
-            console.log(this.currentArray)
+            console.log(this.currentArray, this.store.apiList)
         },
         nextPicCarousel(){
             this.currentArray.shift();
             this.currentArray.push(this.store.apiList[this.currentIndex]);
             this.currentIndex = (this.currentIndex + 1) % this.store.apiList.length;
+            this.backwardIndex = (this.currentIndex - 4 + this.store.apiList.length ) % this.store.apiList.length
+            console.log(this.currentIndex)
         },
         forwardPicCarousel(){
             this.currentArray.pop();
             this.currentArray.unshift(this.store.apiList[this.backwardIndex]);
             this.backwardIndex = (this.backwardIndex - 1 + this.store.apiList.length) % this.store.apiList.length;
+            this.currentIndex = (this.backwardIndex + 4) % this.store.apiList.length
+            console.log(this.backwardIndex)
         }
     },
     created() {
@@ -40,14 +44,22 @@ export default {
 </script>
 
 <template>
-    <div class="container-fluid d-flex align-items-center">
-        <div class="row overflow-x-auto">
-            <div class="d-flex flex-nowrap">
-                <button @click="forwardPicCarousel()"><</button>
-                <li v-for="(pic,index) in currentArray" :key="index" class="card">
+    <div class="d-flex align-items-center">
+        <div class="container position-relative">
+            <div class="row d-flex">
+
+                <li v-for="(pic,index) in currentArray" :key="index" class="card col-3">
                     <img :src="pic.image" :alt="pic.id">
                 </li>
-                <button @click="nextPicCarousel()">></button>
+
+                <span class="left">
+                    <button @click="forwardPicCarousel"><</button>
+                </span>
+
+                <span class="right">
+                    <button @click="nextPicCarousel">></button>
+                </span>
+
             </div>
         </div>
     </div>
@@ -56,4 +68,17 @@ export default {
 <style scoped lang="scss">
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
+
+// .right {
+//     position: absolute;
+//     right: 0;
+//     border-radius: 50%;
+// }
+
+// .left {
+//     position: absolute;
+//     left: 0;
+//     border-radius: 50%;
+// }
+
 </style>
