3df4f99d4ddfda375deb59aaa8bcf043d1d00ea9
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-20 16:36:11 +0200

    added banner

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -8,10 +8,18 @@ export default {
 </script>
 
 <template>
-<div>MainBanner</div>
+<div class="container-fluid">
+    <div class="container">
+        <img src="../assets/header-banner.webp" alt="logo" class="w-100 py-4">
+    </div>
+</div>
 </template>
 
 <style scoped lang="scss">
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
+div.container-fluid{
+    background-color: #f3f3f3;
+}
+
 </style>
