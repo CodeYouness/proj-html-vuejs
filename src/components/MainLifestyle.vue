5ad1611bd841f3d9537911d9ec38aff6640fb9b1
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-20 17:28:36 +0200

    added article info and tags

--- src/components/MainLifestyle.vue
+++ src/components/MainLifestyle.vue
@@ -28,6 +28,9 @@ export default{
         },
         getImagePath: function(img){
             return new URL('./.' + img, import.meta.url).href;
+        },
+        getRandomNumber: function(max){
+            return Math.floor(Math.random() * max);
         }
     },
     created(){
@@ -42,7 +45,7 @@ export default{
         <div class="container">
             <div class="row">
                 <div class="col-9 text-uppercase">
-                    <h3>Lifestyle & stories</h3>
+                    <h3 class="fw-bold fs-4">Lifestyle & stories</h3>
                 </div>
                 <div class="col-3">
                     <input type="radio" name="category-tag" id="tag-all">
@@ -55,12 +58,32 @@ export default{
             </div>
             <div class="article-wrapper row flex-column">
                 <div v-for="(article, index) in randomArticlesArray" :key="article.index" :class="index === 0 ? 'col-8' : 'col-4'">
-                    <article class="border border-primary w-100 d-flex">
-                        <div class="article-image" >
-                            <img :src="getImagePath(article.path)" :alt="article.title" class="w-100">
+                    <article class="w-100 d-flex">
+                        <div class="article-image position-relative" >
+                            <img :src="getImagePath(article.path)" :alt="article.title" class="w-100 h-100 rounded">
+                            <div class="overlay position-absolute rounded top-0 z-1 h-100 w-100">
+                            </div>
+                            <div class="position-absolute z-2 top-0 h-100 w-100 d-flex flex-column align-items-start justify-content-between ps-2 pt-3">
+                                <span class="tag bg-white fw-normal text-black px-3 py-1"> {{ article.category[getRandomNumber(article.category.length)] }}</span>
+                                <div v-if="index == 0" class="article-info fw-bold text-white p-3">
+                                    <p class="mb-0 d-flex justify-content-center align-items-baseline">
+                                        <font-awesome-icon icon="fa-solid fa-user"/>
+                                        <span class="mx-1">{{ article.author }}</span>
+                                        <font-awesome-icon icon="fa-solid fa-calendar-days" class="mx-1"/>
+                                        <span>{{ article.date }}</span>
+                                    </p>
+                                    <p class="mb-0 fs-6"> {{ article.title }} </p>
+                                </div>
+                            </div>
                         </div>
-                        <div v-if="index != 0" class="article-info">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <div v-if="index != 0" class="article-info fw-bold">
+                            <p class="mb-0 d-flex justify-content-center align-items-baseline">
+                                <font-awesome-icon icon="fa-solid fa-user"/>
+                                <span class="mx-1">{{ article.author }}</span>
+                                <font-awesome-icon icon="fa-solid fa-calendar-days" class="mx-1"/>
+                                <span>{{ article.date }}</span>
+                            </p>
+                            <p class="mb-0 fs-6"> {{ article.title }} </p>
                         </div>
                     </article>
                     <hr class="w-100 my-0">
@@ -74,8 +97,16 @@ export default{
 @use '../styles/partials/mixin' as *;
 @use '../styles/partials/variable' as *;
 
+article {
+    cursor: pointer;
+}
+
 .row.article-wrapper {
     height: 450px;
+
+    img {
+        object-fit: cover;
+    }
 
     div.col-8{
         height: 100%;
@@ -85,10 +116,10 @@ export default{
         article{
             height: 98%;
 
-            // .article-image{
-            //     width: 100%;
+            .article-image{
+                width: 100%;
 
-            // }
+            }
         }
 
         hr {
@@ -108,7 +139,23 @@ export default{
             height: 75%;
 
             .article-image {
-                width: 45%;
+                width: 40%;
+            }
+
+            .article-info{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: flex-start;
+                padding-left: .7rem;
+
+                p:first-child{
+                    color: #545454;
+                }
+
+                p> span{
+                    font-size: .9rem;
+                }
             }
         }
 
@@ -127,6 +174,20 @@ export default{
         hr {
             display: none;
         }
+    }
+
+    div.overlay {
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    article:hover .overlay {
+        filter: contrast(50%);
+        transition: all .2s ease-out;
+    }
+
+    .tag {
+        border-radius: 5px;
+        margin: 0 .3rem;
     }
 }
 </style>
